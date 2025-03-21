62c76b563e7302f440e0563675e11b56a8689717
AnnaMelekh <annasmelekhina@gmail.com>
2025-03-21 07:41:35 +0100

    Initial Commit

---
+++ sportApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sportApp
+//
+//  Created by Anna Melekhina on 21.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sportAppTests/sportAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  sportAppTests.swift
+//  sportAppTests
+//
+//  Created by Anna Melekhina on 21.03.2025.
+//
+
+import Testing
+@testable import sportApp
+
+struct sportAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
